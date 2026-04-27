312f698af67b4d5e0d59bfb357a41ea78be0d931
bhavik <bhavik@mail.com>
2026-04-28 02:22:57 +0530

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -601,7 +601,7 @@ export default function Home() {
                           View All <ChevronRight size={12} />
                         </Link>
                       </div>
-                      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8">
+                      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
                         {group.products.slice(0, 4).map((product) => (
                           <ProductCard
                             key={product.id}
